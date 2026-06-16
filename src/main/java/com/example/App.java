fb6e25db18e59208ac7bc42282b0b1f8da656c15
Shukruth-Gowda <gshukruth@gmail.com>
2026-06-16 21:53:56 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -5,6 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App{
 	public static void main( String[] args){
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");   // VERY IMPORTANT
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 		
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.saucedemo.com/");
